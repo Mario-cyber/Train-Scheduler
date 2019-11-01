7f08ebd2fa5de40b0c1a8be488c5cdabc852eb42
Mario_95 <mrojasc95.mr@gmail.com>
2019-11-01 17:46:27 -0400

    children appended to database
    children appended to database and into retreived from firebase and stored locally in variables through a child_added event. original obeject submmited to firebase not sucessfully logged into console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,27 +17,59 @@ let database = firebase.database();
 
 $(document).ready(function () {
 
+    // Initial Values
+    let trainName = "";
+    let destination = "";
+    let firstTrain = "";
+    let trainInterval = 0;
+
     // get input from user 
 
     $(".btn").on("click", function () {
         event.preventDefault()
-        let trainName = $("#train").val().trim().toLowerCase()
-        let destination = $("#destination").val().trim().toLowerCase()
-        let firstTrain = $("#first").val().trim().toLowerCase()
-        let trainInterval = $("#interval").val().trim().toLowerCase()
-        $(".form-control").val("")
-        console.log(trainName)
-        console.log(destination)
-        console.log(firstTrain)
-        console.log(trainInterval)
-        database.ref().set({
+        trainName = $("#train").val().trim().toLowerCase()
+        destination = $("#destination").val().trim().toLowerCase()
+        firstTrain = $("#first").val().trim().toLowerCase()
+        trainInterval = $("#interval").val().trim().toLowerCase()
+
+        // create a local object to store train data
+        newTrain = {
             name: trainName,
             destination: destination,
             firstTrain: firstTrain,
             interval: trainInterval
-        })
+        }
+        // console log new object
+        // console.log("new train: " + newTrain)
+
+        // upload train data onto firbase database
+        database.ref().push(newTrain);
+
+        // empty the value of out input bars once info has been submmited
+        $(".form-control").val("")
     })
 
+
+    //  Create Firebase event for adding train data to the database and a row in the html when a user adds an entry
+    //  like an event listenr but for riebase. In this case, when a new child is added to
+    //  firebase, the following comands are executed. 
+
+    database.ref().on("child_added", function (childSnapshot) {
+        console.log("snapshot" + childSnapshot.val());
+
+        // Store everything into a variable.
+        var trainName = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var firstTrain = childSnapshot.val().firstTrain;
+        var trainInterval = childSnapshot.val().interval;
+
+        // log infor into console 
+        console.log(trainName)
+        console.log(destination)
+        console.log(firstTrain)
+        console.log(trainInterval)
+
+    })
 
     // table row generator 
 
@@ -55,11 +87,6 @@ $(document).ready(function () {
 
         // create validation for input values
     }
-
-    console.log("hello")
-
-
-
 
 
 })
